2a27246b9f018f86cf463f9fa1f81b146335c841
Keith Zhang <keithohno@gmail.com>
2023-05-16 14:55:39 -0400

    KZ-24: added loss function

--- examples/main.rs
+++ examples/main.rs
@@ -4,10 +4,16 @@ fn main() {
     let input = NeuronLayer::from_vec(vec![0.0, 1.0, 2.0]);
     let weights: WeightLayer = WeightLayer::new(
         vec![vec![0.0, 1.0, 2.0], vec![3.0, 4.0, 5.0]],
-        vec![0.0, 1.0],
+        vec![5.0, 0.0],
     );
     let output = weights.forward(&input);
+    let softmaxed = output.softmax();
+    let big_loss = softmaxed.cross_entropy_loss(NeuronLayer::from_vec(vec![0.5, 0.5]));
+    let small_loss = softmaxed.cross_entropy_loss(NeuronLayer::from_vec(vec![0.01, 0.99]));
     println!("input: {:?}", input);
     println!("weights: {:?}", weights);
     println!("output: {:?}", output);
+    println!("softmaxed: {:?}", softmaxed);
+    println!("big_loss: {:?}", big_loss);
+    println!("small_loss: {:?}", small_loss);
 }

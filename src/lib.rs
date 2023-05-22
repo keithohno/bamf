ad7e0c3a7b1d20f41a8246d37db8bba2c4053e6e
Keith Zhang <35638530+keithohno@users.noreply.github.com>
2023-05-22 09:56:59 -0400

    KZ-30: Add bias to training (#7)
    * KZ-30: refactored NeuronLayer & Vec<f64> into unified Vector struct
    
    * KZ-30: added biases to training

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,40 @@
 use matrix::{Matrix, Multiply, Scale};
-use misc::subtract;
+use vector::Vector;
 
 pub mod matrix;
-pub mod misc;
-
-#[derive(Debug)]
-pub struct NeuronLayer {
-    dim: usize,
-    vals: Vec<f64>,
-}
-
-impl NeuronLayer {
-    fn new(dim: usize) -> NeuronLayer {
-        NeuronLayer {
-            dim: dim,
-            vals: vec![0.0; dim],
-        }
-    }
-
-    pub fn from_vec(vals: Vec<f64>) -> NeuronLayer {
-        let dim = vals.len();
-        NeuronLayer { dim, vals }
-    }
-
-    pub fn softmax(&self) -> NeuronLayer {
-        let exp_sum = self.vals.iter().map(|x| x.exp()).sum::<f64>();
-        NeuronLayer {
-            dim: self.dim,
-            vals: self.vals.iter().map(|x| x.exp() / exp_sum).collect(),
-        }
-    }
-
-    pub fn cross_entropy_loss(&self, expected: &NeuronLayer) -> f64 {
-        let cross_entropy_fn = |expected: f64, actual: f64| -expected * actual.ln();
-        expected
-            .vals
-            .iter()
-            .zip(self.vals.iter())
-            .map(|(e, a)| cross_entropy_fn(*e, *a))
-            .sum::<f64>()
-    }
-}
+pub mod vector;
 
 #[derive(Debug)]
 pub struct WeightLayer {
-    dim: (usize, usize),
     pub weights: Matrix,
-    biases: Vec<f64>,
+    pub biases: Vector,
 }
 
 impl WeightLayer {
-    pub fn new(weights: Matrix, biases: Vec<f64>) -> WeightLayer {
+    pub fn new<V>(weights: Matrix, biases: V) -> WeightLayer
+    where
+        V: Into<Vector>,
+    {
+        let biases = biases.into();
         assert!(weights.dims[1] == biases.len());
-        WeightLayer {
-            dim: (weights.dims[0], weights.dims[1]),
-            weights,
-            biases,
-        }
+        WeightLayer { weights, biases }
     }
 
-    pub fn forward(&self, input: &NeuronLayer) -> NeuronLayer {
-        let product = self.weights.transpose().multiply(&input.vals);
-        NeuronLayer::from_vec(product)
+    pub fn forward(&self, input: &Vector) -> Vector {
+        self.weights.transpose().multiply(&input).add(&self.biases)
     }
 }
 
 pub struct NeuralNetwork<'a> {
     pub layer: &'a mut WeightLayer,
-    input: &'a NeuronLayer,
-    pub intermediates: Vec<NeuronLayer>,
-    target: NeuronLayer,
+    input: &'a Vector,
+    pub intermediates: Vec<Vector>,
+    target: Vector,
     pub loss: f64,
 }
 
 impl<'a> NeuralNetwork<'a> {
-    pub fn new(
-        layer: &'a mut WeightLayer,
-        input: &'a NeuronLayer,
-        target: NeuronLayer,
-    ) -> NeuralNetwork<'a> {
+    pub fn new(layer: &'a mut WeightLayer, input: &'a Vector, target: Vector) -> NeuralNetwork<'a> {
         NeuralNetwork {
             layer,
             input,
@@ -100,23 +56,24 @@ impl<'a> NeuralNetwork<'a> {
             .cross_entropy_loss(&self.target);
     }
 
-    pub fn backward(&mut self) -> Matrix {
-        let dl_dy = subtract(&self.intermediates.last().unwrap().vals, &self.target.vals);
-        let dvec = (0..self.input.vals.len())
-            .map(|x| vec![self.input.vals[x]; self.target.vals.len()])
-            .collect::<Vec<Vec<f64>>>();
-        let mut dw_dy = Matrix::empty(vec![self.input.vals.len(), self.target.vals.len()]);
-        for i in 0..dw_dy.dims[0] {
-            for j in 0..dw_dy.dims[1] {
-                dw_dy.data[i * dw_dy.step[0] + j * dw_dy.step[1]] = self.input.vals[i]
+    pub fn backward(&mut self) -> (Matrix, Vector) {
+        let dl_dy = self.intermediates.last().unwrap().subtract(&self.target);
+        let mut dy_dw = Matrix::empty(vec![self.input.vals.len(), self.target.vals.len()]);
+        for i in 0..dy_dw.dims[0] {
+            for j in 0..dy_dw.dims[1] {
+                dy_dw.data[i * dy_dw.step[0] + j * dy_dw.step[1]] = self.input.vals[i]
             }
         }
-        dw_dy.transpose().scale(&dl_dy)
+        let dy_db = Vector::from(vec![1.0; self.target.vals.len()]);
+        let dl_dw = dy_dw.transpose().scale(&dl_dy);
+        let dl_db = dy_db.scale(&dl_dy);
+        (dl_dw, dl_db)
     }
 
     pub fn train(&mut self) {
         self.forward();
-        let dw_dy = self.backward();
-        self.layer.weights = self.layer.weights.subtract(&dw_dy.transpose());
+        let (dl_dw, dl_db) = self.backward();
+        self.layer.weights = self.layer.weights.subtract(&dl_dw.transpose());
+        self.layer.biases = self.layer.biases.subtract(&dl_db);
     }
 }
--- src/misc.rs
+++
@@ -1,4 +0,0 @@
-pub fn subtract(a: &Vec<f64>, b: &Vec<f64>) -> Vec<f64> {
-    assert!(a.len() == b.len());
-    a.iter().zip(b.iter()).map(|(x, y)| x - y).collect()
-}

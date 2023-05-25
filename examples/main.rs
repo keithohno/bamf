2fe81ba5d96ca80a2624cce891309a161438eb3f
Keith Zhang <35638530+keithohno@users.noreply.github.com>
2023-05-24 21:51:01 -0400

    KZ-34: Add nontrivial geometric training examples (#11)
    * KZ-34: made target specific to training instances
    
    * KZ-34: added point in circle example
    
    * KZ-34: added second geometric training example

--- examples/main.rs
+++ examples/main.rs
@@ -10,12 +10,11 @@ fn main() {
 fn repeat_nn_single_input() {
     let weights1 = Layer::random((3, 5), (-0.5, 1.0));
     let weights2 = Layer::random((5, 2), (-0.5, 1.0));
-    let mut nn = NeuralNetwork::new(vec![weights1, weights2], Vector::from(vec![0.0, 1.0]));
+    let mut nn = NeuralNetwork::new(vec![weights1, weights2]);
     for _ in 0..20 {
         let input = Vector::from(vec![0.0, 1.0, 2.0]);
-        nn.train(input);
-        println!("loss: {:?}", nn.loss);
+        let target = Vector::from(vec![0.0, 1.0]);
+        let loss = nn.train(input, &target);
+        println!("loss: {:?}", loss);
     }
-    println!("loss: {:?}", nn.loss);
-    println!("output: {:?}", nn.intermediates.last().unwrap().softmax());
 }

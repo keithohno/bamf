aa5e004ec4e33df416b6a44d6a7b8201e188c40b
Keith Zhang <keithohno@gmail.com>
2023-05-27 17:03:12 -0400

    KZ-44: generate one-hot encoded word pairs

--- examples/main.rs
+++ examples/main.rs
@@ -6,4 +6,5 @@ fn main() {
     let input_text = read_to_string("examples/your_name.txt").unwrap();
     let embedding_builder = EmbeddingBuilder::new(input_text);
     println!("{:?}", embedding_builder);
+    println!("{:?}", embedding_builder.random_pairing());
 }
--- src/language.rs
+++ src/language.rs
@@ -1,3 +1,4 @@
+use rand::{thread_rng, Rng};
 use regex::Regex;
 use std::collections::HashMap;
 
@@ -6,17 +7,43 @@ pub struct EmbeddingBuilder {
     vocab: HashMap<String, usize>,
     vocab_size: usize,
     codex: Vec<usize>,
+    codex_size: usize,
+    window: usize,
 }
 
 impl EmbeddingBuilder {
     pub fn new(input: String) -> EmbeddingBuilder {
         let (codex, vocab) = tokenize(clean(input));
         let vocab_size = vocab.len();
+        let codex_size = codex.len();
         EmbeddingBuilder {
             vocab,
             vocab_size,
             codex,
+            codex_size,
+            window: 1,
         }
+    }
+
+    pub fn set_window(&mut self, window: usize) {
+        self.window = window;
+    }
+
+    pub fn random_pairing(&self) -> (Vec<f64>, Vec<f64>) {
+        let mut rng = thread_rng();
+        let offset = rng.gen_range(1..=self.window);
+        let index = rng.gen_range(0..(self.codex_size - offset));
+        let (num1, num2) = match rng.gen::<bool>() {
+            true => (self.codex[index], self.codex[index + offset]),
+            false => (self.codex[index + offset], self.codex[index]),
+        };
+        (self.one_hot(num1), self.one_hot(num2))
+    }
+
+    pub fn one_hot(&self, num: usize) -> Vec<f64> {
+        let mut one_hot = vec![0.0; self.vocab_size];
+        one_hot[num] = 1.0;
+        one_hot
     }
 }
 

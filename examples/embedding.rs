ae76ae9a95b3e3e870fa890db8ac015cc571e368
Keith Zhang <keithohno@gmail.com>
2023-06-05 22:28:57 -0400

    KZ-52: added user-input to embedding example

--- examples/embedding.rs
+++ examples/embedding.rs
@@ -1,16 +1,30 @@
 use std::fs::read_to_string;
+use std::io;
 
 use bamf::language::Embedding;
 
 fn main() {
     let input_text = read_to_string("examples/your_name.txt").unwrap();
     let embedding = Embedding::builder(input_text)
-        .window(2)
+        .window(5)
         .dim(10)
-        .train(50000);
-    println!("{:?}", embedding.get("your").unwrap());
-    println!("{:?}", embedding.get("name").unwrap());
-    println!("{:?}", embedding.get("taki").unwrap());
-    println!("{:?}", embedding.get("mitsuha").unwrap());
-    println!("{:?}", embedding.get("asdfasdf"));
+        .train(1000000);
+    loop {
+        let mut word1 = String::new();
+        let mut word2 = String::new();
+
+        println!("word 1: ");
+        io::stdin().read_line(&mut word1).unwrap();
+        println!("word 2: ");
+        io::stdin().read_line(&mut word2).unwrap();
+
+        let embedding1 = embedding.get(&word1.trim());
+        let embedding2 = embedding.get(&word2.trim());
+        match (embedding1, embedding2) {
+            (Some(embedding1), Some(embedding2)) => {
+                println!("similarity: {}", embedding1.dot(&embedding2));
+            }
+            _ => println!("word not found"),
+        }
+    }
 }
